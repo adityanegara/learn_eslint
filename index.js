21d7d4bba3d10c995f6063b5eb149cd9726318c2
adityanegara <aditnegarabisnis@gmail.com>
2021-06-13 15:32:44 +0700

    Initial Work

---
+++ index.js
@@ -0,0 +1,11 @@
+let didShowMessage = false;
+
+function showMessage(){
+    const message = "Hello World";
+
+    for(let i = 0; i< 5; i++){
+        console.log(message);
+    }
+}
+
+showMessage();
